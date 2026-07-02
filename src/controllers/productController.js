d8e55d4e805fb73ed95b494c802f321eedb3e09c
Muhammad Farhan Al Ghifari <al.ghifarifarhan2503@gmail.com>
2026-07-02 10:27:01 +0700

    update: productController.js

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -72,7 +72,7 @@ exports.updateProduct = async (req, res) => {
         });
         res.status(200).json({ success: true, data: product });
     } catch (error) {
-        // P2025 adalah kode error Prisma untuk Record Not Found
+        // Kode error Prisma untuk Record Not Found
         if (error.code === 'P2025') {
             return res.status(404).json({ success: false, message: 'Produk tidak ditemukan' });
         }
