10734a2b0d80b70e149849c653dd5941e4426beb
Muhammad Farhan Al Ghifari <al.ghifarifarhan2503@gmail.com>
2026-07-02 11:30:09 +0700

    update: app.js v2

--- src/app.js
+++ src/app.js
@@ -4,16 +4,13 @@ const productRoute = require('./routes/productRoute');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Mengaktifkan format JSON response (Pretty-Print) agar rapi dibaca di browser
+
 app.set('json spaces', 2);
 
-// Middleware internal untuk parsing request body berformat JSON
 app.use(express.json());
 
-// Registrasi router spesifik dengan penentuan prefix rute global /api
 app.use('/api', productRoute);
 
-// Inisialisasi proses mendengarkan (listening) request pada port target
 app.listen(PORT, () => {
     console.log(`Server Express berjalan secara optimal di http://localhost:${PORT}`);
 });
